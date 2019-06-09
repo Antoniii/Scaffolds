6d3a600d6ab6d3c2318602e6329faacd0c197b87
Vova Abdrakhmanov <369565@gmail.com>
2019-06-09 16:12:08 +0400

    Minor improvements

--- scaffold3.py
+++ scaffold3.py
@@ -1,47 +1,101 @@
 #!/usr/bin/python3
-# -*- encoding: utf-8 -*-
-import random
+'''
+       __       __  
+      | _|     |_ |        ___       _  _               
+      | |   _   | |       / __| ___ (_)| | ___  _ _  ___
+      | |  (_)  | |      | (__ / -_)| || |/ _ \| '_|(_-<
+      | |       | |       \___|\___||_||_|\___/|_|  /__/                               
+      |_|       |_| 
 
-def hangman():
-    #word_list = ["Тони", "Старк", "умрёт", "в", "финале"]
-    word_list = []
-    with open('word_rus.txt') as f:
-        word_list = f.read().splitlines()
-    random_number = random.randint(0, len(word_list))
-    word = word_list[random_number]
-    #print(word)
-    wrong_guesses = 0
-    stages = ["",
-             "________        ",
-             "|               ",
-             "|        |      ",
-             "|        0      ",
-             "|       /|\     ",
-             "|       / \     ",
-             "|               "
-              ]
-    remaining_letters = list(word)
-    letter_board = ["__"] * len(word)
+
+                    HANGMAN REVENGE v0.666
+                     --= TONY EDITION =--
+
+
+                      ***  CREDITS: ***
+
+                 ORIGINAL AUTHOR: Tony Adams
+                    ART DIRECTOR: Alex Courlyck
+                  HEAD DEVELOPER: Eli Stepper
+                    CODE STYLER,
+                  LEAD DEVELOPER: Johnny Abdrams
+
+                     (c) Ceilors, 2k19
+
+'''
+
+import random
+import re
+from typing import Set
+
+regex = re.compile('^[а-я]$')
+stages = [
+    '________        ',
+    '|        |      ',
+    '|        |      ',
+    '|        |      ',
+    '|        |      ',
+    '|        0      ',
+    '|       /|\     ',
+    '|       / \     ',
+    '|               '
+]
+
+
+def mask_word(word: str, masked_letters: Set[str]) -> str:
+    '''
+
+    Parameters
+    ----------
+
+        word : str
+            word needs to be masked
+
+        masked_letters : Set[str]
+            letters should be masked
+
+    Returns
+    -------
+
+        masked word : str
+    '''
+    return ''.join(['_' if c in masked_letters else c for c in list(word)])
+
+
+def hangman(word: str):
+    word = word.lower()
+    wrong_guesses = set()
+    remaining_letters = set(list(word))
     win = False
     print('Добро пожаловать на казнь русских слов!')
-    while wrong_guesses < len(stages) - 1:
-        print('\n')
-        guess = input("Введите букву: ")
+    while len(wrong_guesses) < len(stages):
+        guess = input('Введите букву: ').lower()
+        if not regex.match(guess):
+            print("Используй русские буквы")
+            continue
+        print('')
         if guess in remaining_letters:
-            character_index = remaining_letters.index(guess)
-            letter_board[character_index] = guess
-            remaining_letters[character_index] = '$'
+            remaining_letters.discard(guess)
         else:
-            wrong_guesses += 1
-        print((' '.join(letter_board)))
-        print('\n'.join(stages[0: wrong_guesses + 1]))
-        if '__' not in letter_board:
-            print('Вы выиграли! Было загадано слово:')
-            print(' '.join(letter_board))
+            wrong_guesses.add(guess)
+
+        if wrong_guesses:
+            print(f'Неудачные попытки: {" ".join(wrong_guesses)}')
+
+        print(mask_word(word, remaining_letters))
+        print('\n'.join(stages[:len(wrong_guesses)]))
+
+        if not remaining_letters:
             win = True
             break
     if not win:
-        print('\n'.join(stages[0: wrong_guesses]))
-        print('Вы проиграли! Было загадано слово: {}'.format(word))
+        print('\n'.join(stages))
+        print(f'Вы проиграли! Было загадано слово: {word}')
+    else:
+        print(f'Вы выиграли! Было загадано слово: {word}')
 
-hangman()
+
+if __name__ == '__main__':
+    with open('word_rus.txt') as f:
+        words = f.readlines()
+        hangman(random.choice(words).strip())
